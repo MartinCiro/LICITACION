c9cec20c0a4adc980b76eeb2f5ee918c12197848
martinciro <martinciro11@gmail.com>
2023-09-05 00:08:04 -0500

    se agrega funcion en api para evitar duplicidad de codigo, se actualiza posicion de listado de habitaciones disponibles

--- src/modules/clientes/api/clientes.api.js
+++ src/modules/clientes/api/clientes.api.js
@@ -1,85 +1,46 @@
 const { validar, validarUser, crearUser, delUser, updateUser, obtenerProductos} = require("../controller/clientes.controller");
 const ResponseBody = require('../../../shared/model/ResponseBody.model');
 
-const crearUserApi = async (req, res) => {
+const fnApi = async (req, res, apiFunction) => {
+    try {
+        const result = await apiFunction(req.body);
+        const message = new ResponseBody(true, 200, result);
+        return res.status(message.status_cod).json(message);
+    } catch (error) {
+        if (error.data) {
+            const message = new ResponseBody(error.ok, error.status_cod, error.data);
+            return res.status(message.status_cod).json(message);
+        } else {
+            const message = new ResponseBody(false, 500, { message: 'Ha ocurrido un error inesperado. Por favor inténtelo nuevamente más tarde' });
+            return res.status(message.status_cod).json(message);
+        }
+    }
+};
+//Inicio CRUD Usuario
+const crearUserApi = (req, res) => {
     const { nombre, num_cedula, pais, departamento, ciudad, pass } = req.body;
-    console.log("Entrando a controller");
-    try {
-        const crearUserv = await crearUser(nombre, num_cedula, pais, departamento, ciudad, pass);
-        message = new ResponseBody(true, 200, crearUserv);
-    } catch (error) {
-        if (error.data) {
-            message = new ResponseBody(error.ok, error.status_cod, error.data);
-        } else {
-            message = new ResponseBody(false, 500, { message: 'Ha ocurrido un error inesperado. Por favor inténtelo nuevamente más tarde' });
-        }
-    }
+    return fnApi(req, res, crearUser.bind(null, nombre, num_cedula, pais, departamento, ciudad, pass));
+};
 
-    return res.status(message.status_cod).json(message);
-}
-
-const validarUserApi = async (req, res) => {
+const validarUserApi = (req, res) => {
     const { num_cedula, pass } = req.body;
-    try {
-        const validarUserv = await validarUser(num_cedula, pass);
-        message = new ResponseBody(true, 200, validarUserv);
-    } catch (error) {
-        if (error.data) {
-            message = new ResponseBody(error.ok, error.status_cod, error.data);
-        } else {
-            message = new ResponseBody(false, 500, { message: 'Ha ocurrido un error inesperado. Por favor inténtelo nuevamente más tarde' });
-        }
-    }
+    return fnApi(req, res, validarUser.bind(null, num_cedula, pass));
+};
 
-    return res.status(message.status_cod).json(message);
-    
-}
-
-const delUserApi = async (req, res) => {
-    const { num_cedula } = req.body;
-    console.log("Entrando a controller");
-    try {
-        const delUserv = await delUser(num_cedula);
-        message = new ResponseBody(true, 200, delUserv);
-    } catch (error) {
-        if (error.data) {
-            message = new ResponseBody(error.ok, error.status_cod, error.data);
-        } else {
-            message = new ResponseBody(false, 500, { message: 'Ha ocurrido un error inesperado. Por favor inténtelo nuevamente más tarde' });
-        }
-    }
-    return res.status(message.status_cod).json(message);
-}
-const updateUserApi = async (req, res) => {
+const updateUserApi = (req, res) => {
     const { nombre, num_cedula, pais, departamento, ciudad } = req.body;
-    console.log("Entrando a controller");
-    try {
-        const updateUserV = await updateUser(nombre, num_cedula, pais, departamento, ciudad);
-        message = new ResponseBody(true, 200, updateUserV);
-    } catch (error) {
-        if (error.data) {
-            message = new ResponseBody(error.ok, error.status_cod, error.data);
-        } else {
-            message = new ResponseBody(false, 500, { message: 'Ha ocurrido un error inesperado. Por favor inténtelo nuevamente más tarde' });
-        }
-    }
-    return res.status(message.status_cod).json(message);
-}
+    return fnApi(req, res, updateUser.bind(null, nombre, num_cedula, pais, departamento, ciudad));
+};
 
-const obtenerProductosApi = async (req, res) => {
-    console.log("Entrando a controller");
-    try {
-        const obtenerProductosV = await obtenerProductos();
-        message = new ResponseBody(true, 200, obtenerProductosV);
-    } catch (error) {
-        if (error.data) {
-            message = new ResponseBody(error.ok, error.status_cod, error.data);
-        } else {
-            message = new ResponseBody(false, 500, { message: 'Ha ocurrido un error inesperado. Por favor inténtelo nuevamente más tarde' });
-        }
-    }
-    return res.status(message.status_cod).json(message);  
-}
+const delUserApi = (req, res) => {
+    const { num_cedula } = req.body;
+    return fnApi(req, res, delUser.bind(null, num_cedula));
+};
+//Fin CRUD Usuario
+
+const obtenerProductosApi = (req, res) => {
+    return fnApi(req, res, obtenerProductos);
+};
 
 module.exports = {
     validar,
